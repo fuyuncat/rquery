6e3069e5c0ed7b58160857bb96af473575ccc80e
fuyuncat <fuyuncat@gmail.com>
2022-06-26 15:29:52 +1000

    naming smatch

--- commfuncs.cpp
+++ commfuncs.cpp
@@ -14,33 +14,33 @@
 #include <stdio.h>
 #include "commfuncs.h"
 
-namesaving_smatch::namesaving_smatch(const std::string pattern)
+namesaving_smatch::namesaving_smatch(const string pattern)
 {
-    std::string pattern_str = pattern;
+    string pattern_str = pattern;
     sregex capture_pattern = sregex::compile("\\?P?<(\\w+)>");
     sregex_iterator words_begin = sregex_iterator(pattern_str.begin(), pattern_str.end(), capture_pattern);
     sregex_iterator words_end = sregex_iterator();
 
     for (sregex_iterator i = words_begin; i != words_end; i++)
     {
-        std::string name = (*i)[1].str();
+        string name = (*i)[1].str();
         m_names.push_back(name);
     }
 }
 
 namesaving_smatch::~namesaving_smatch() { }
 
-namesaving_smatch::vector<std::string>::const_iterator names_begin() const
+namesaving_smatch::vector<string>::const_iterator names_begin() const
 {
     return m_names.begin();
 }
 
-namesaving_smatch::vector<std::string>::const_iterator names_end() const
+namesaving_smatch::vector<string>::const_iterator names_end() const
 {
     return m_names.end();
 }
 
-namesaving_smatch::vector<std::string> m_names;
+namesaving_smatch::vector<string> m_names;
 
 
 vector<string> split(string str, char delim, char quoter, char escape) 
@@ -51,7 +51,7 @@ vector<string> split(string str, char delim, char quoter, char escape)
   while(i < str.size()) {
     if (str[i] == delim && i>0 && !quoted) {
       printf("found delim\n");
-      v.push_back(std::string(str, begin, i));
+      v.push_back(string(str, begin, i));
       begin = i+1;
     }
     if (str[i] == quoter) {
@@ -62,14 +62,14 @@ vector<string> split(string str, char delim, char quoter, char escape)
     ++i;
   }
   if (begin<str.size())
-    v.push_back(std::string(str, begin, str.size()));
+    v.push_back(string(str, begin, str.size()));
 
   /*while(i < str.size()) {
     if(str[i] == delim || i == 0) {
       if(i + 1 < str.size() && str[i + 1] == escape) {
         j = begin + 1;
         while(j < str.size() && str[j++] != escape);
-          v.push_back(std::string(str, begin, j - 1 - i));
+          v.push_back(string(str, begin, j - 1 - i));
         begin = j - 1;
         i = j - 1;
         continue;
@@ -77,7 +77,7 @@ vector<string> split(string str, char delim, char quoter, char escape)
 
       j = begin + 1;
       while(j < str.size() && str[j++] != delim);
-      v.push_back(std::string(str, begin, j - 1 - i - (i ? 1 : 0) ));
+      v.push_back(string(str, begin, j - 1 - i - (i ? 1 : 0) ));
       begin = j;
     }
     ++i;

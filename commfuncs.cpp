55f614da03ec07b0f605650f240adeff116f041a
fuyuncat <fuyuncat@gmail.com>
2022-06-26 15:28:14 +1000

    comm funcs

--- commfuncs.cpp
+++ commfuncs.cpp
@@ -14,38 +14,33 @@
 #include <stdio.h>
 #include "commfuncs.h"
 
-class namesaving_smatch : public smatch
+namesaving_smatch::namesaving_smatch(const std::string pattern)
 {
-public:
-    namesaving_smatch(const std::string pattern)
+    std::string pattern_str = pattern;
+    sregex capture_pattern = sregex::compile("\\?P?<(\\w+)>");
+    sregex_iterator words_begin = sregex_iterator(pattern_str.begin(), pattern_str.end(), capture_pattern);
+    sregex_iterator words_end = sregex_iterator();
+
+    for (sregex_iterator i = words_begin; i != words_end; i++)
     {
-        std::string pattern_str = pattern;
-        sregex capture_pattern = sregex::compile("\\?P?<(\\w+)>");
-        sregex_iterator words_begin = sregex_iterator(pattern_str.begin(), pattern_str.end(), capture_pattern);
-        sregex_iterator words_end = sregex_iterator();
-
-        for (sregex_iterator i = words_begin; i != words_end; i++)
-        {
-            std::string name = (*i)[1].str();
-            m_names.push_back(name);
-        }
+        std::string name = (*i)[1].str();
+        m_names.push_back(name);
     }
+}
 
-    ~namesaving_smatch() { }
+namesaving_smatch::~namesaving_smatch() { }
 
-    std::vector<std::string>::const_iterator names_begin() const
-    {
-        return m_names.begin();
-    }
+namesaving_smatch::vector<std::string>::const_iterator names_begin() const
+{
+    return m_names.begin();
+}
 
-    std::vector<std::string>::const_iterator names_end() const
-    {
-        return m_names.end();
-    }
+namesaving_smatch::vector<std::string>::const_iterator names_end() const
+{
+    return m_names.end();
+}
 
-private:
-    std::vector<std::string> m_names;
-};
+namesaving_smatch::vector<std::string> m_names;
 
 
 vector<string> split(string str, char delim, char quoter, char escape) 

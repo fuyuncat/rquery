d84d5e6dba86b1458d52515c155a72b6e96daff9
fuyuncat <fuyuncat@gmail.com>
2022-06-26 18:22:21 +1000

    Querier

--- querierc.cpp
+++ querierc.cpp
@@ -54,11 +54,11 @@ int QuerierC::boostmatch(vector<string> *result)
   //smatch matches;
   //boost::match_results<std::string::const_iterator>  matches;
   if ( result != NULL ) {
-    printf("Matching %s => %s\n",m_rawstr.c_str(), m_regexp.c_str());
+    //printf("Matching %s => %s\n",m_rawstr.c_str(), m_regexp.c_str());
     result->clear();
     //if (boost::regex_match(m_rawstr, matches, m_rexp, boost::match_perl|boost::match_extra)) {
     if (regex_match(m_rawstr, matches, m_rexp)) {
-      printf("Matched %d!\n", matches.size());
+      //printf("Matched %d!\n", matches.size());
       //for (std::vector<std::string>::const_iterator it = matches.names_begin(); it != matches.names_end(); ++it)
       //  printf("%s: %s\n",string(*it).c_str(),matches[*it].str().c_str());
       for (int i=1; i<matches.size(); i++){
@@ -76,7 +76,7 @@ int QuerierC::boostmatch(vector<string> *result)
 int QuerierC::boostmatch(map<string,string> & result)
 {
   namesaving_smatch matches(m_regexp);
-  printf("Matching %s => %s\n",m_rawstr.c_str(), m_regexp.c_str());
+  //printf("Matching %s => %s\n",m_rawstr.c_str(), m_regexp.c_str());
   if (regex_match(m_rawstr, matches, m_rexp)) {
     for (std::vector<std::string>::const_iterator it = matches.names_begin(); it != matches.names_end(); ++it)
       result[string(*it)] = matches[*it].str();

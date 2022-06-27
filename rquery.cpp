3aae9af1cbc827c80d9ecc6fe5cfcf0dcd45df44
fuyuncat <fuyuncat@gmail.com>
2022-06-27 16:24:07 +1000

    Parser Class

--- rquery.cpp
+++ rquery.cpp
@@ -78,8 +78,8 @@ int main(int argc, char *argv[])
     bool namePrinted = false;
     string lineInput;
     int ln = 0;
-    //while (cin >> lineInput) {
-    while (getline(cin,lineInput)) {
+    while (cin >> lineInput) {
+    //while (getline(cin,lineInput)) {
       //printf("%d:%s\n",ln,lineInput.c_str());
       rq.setrawstr(lineInput);
       rq.searchAll();

5b69fdca2bd700502a906580e46d34828f50341a
fuyuncat <fuyuncat@gmail.com>
2022-06-29 09:06:56 +1000

    Parser

--- parser.cpp
+++ parser.cpp
@@ -30,9 +30,10 @@ ParserC::~ParserC()
 void ParserC::init()
 {
   analyzedPos = 0;
-  vector<string> ParserC::junctionWords; junctionWords.push_back("AND"); junctionWords.push_back("OR");
-  vector<string> ParserC::junctionSplitors; junctionSplitors.push_back(" AND ");junctionSplitors.push_back(" OR ");
-  vector<string> ParserC::comparators; comparators.push_back("=");comparators.push_back("!=");comparators.push_back(">=");comparators.push_back("<=");comparators.push_back(">");comparators.push_back("<"); // ">=", "<=" should be before ">", "<"
+  ParserC::junctionWords.push_back("AND");
+  //vector<string> ParserC::junctionWords; junctionWords.push_back("AND"); junctionWords.push_back("OR");
+  //vector<string> ParserC::junctionSplitors; junctionSplitors.push_back(" AND ");junctionSplitors.push_back(" OR ");
+  //vector<string> ParserC::comparators; comparators.push_back("=");comparators.push_back("!=");comparators.push_back(">=");comparators.push_back("<=");comparators.push_back(">");comparators.push_back("<"); // ">=", "<=" should be before ">", "<"
 }
 
 // return operation type: -1 error; 0: unused; 1: parse; 2:select; 3: filter; 4: group; 5: sort

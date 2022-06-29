b18c1f889c4cdf04b69de7b25a3d57e3f8a1edb1
fuyuncat <fuyuncat@gmail.com>
2022-06-30 09:57:06 +1000

    Matching

--- rquery.cpp
+++ rquery.cpp
@@ -90,7 +90,7 @@ int main(int argc, char *argv[])
     GlobalVars gv;
     gv.setVars(16384*2, FATAL);
 
-    //printf("g_inputbuffer:%d\n",GlobalVars::g_inputbuffer);
+    trace(FATAL, "g_inputbuffer:%d\n",GlobalVars::g_inputbuffer);
     const size_t cache_length = GlobalVars::g_inputbuffer;
     char cachebuffer[cache_length];
     size_t howmany = 0, reads = 0;

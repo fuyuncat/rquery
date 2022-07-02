ce9459ea3324ad1f2f79ad47b602137ebef72e11
fuyuncat <fuyuncat@gmail.com>
2022-07-03 09:27:08 +1000

    Expression improvement

--- commfuncs.cpp
+++ commfuncs.cpp
@@ -91,7 +91,7 @@ void trace(short level, const char *fmt, ...)
   va_list args;
   va_start(args, fmt);
   if (GlobalVars::g_tracelevel>=level){
-    vprintf(decodeTracelevel(level)+":");
+    printf(decodeTracelevel(level)+":");
     vprintf(fmt, args);
   }
   va_end(args);

a62f01cf3d845b0965b41c276e9b6588bd628f3b
fuyuncat <fuyuncat@gmail.com>
2022-07-04 18:02:20 +1000

    Expression improvement

--- querierc.cpp
+++ querierc.cpp
@@ -126,8 +126,10 @@ void QuerierC::analyzeFiledTypes(namesaving_smatch matches)
       m_fieldtypes.push_back(m_fieldntypes[m_fieldnames[i-1]]);
     else if (m_fieldntypes.find("@FIELD"+intToStr(i)) != m_fieldntypes.end())
       m_fieldtypes.push_back(m_fieldntypes["@FIELD"+intToStr(i)]);
-    else
-      m_fieldtypes.push_back(detectDataType(matches[i]));
+    else{
+      int iType = detectDataType(matches[i];
+      m_fieldtypes.push_back(iType==UNKNOWN?STRING:iType)); // set UNKNOWN (real) data as STRING
+    }
     trace(DEBUG, "Detected column '%s' data type '%s'\n", m_fieldnames[i-1].c_str(), decodeDatatype(m_fieldtypes[m_fieldtypes.size()-1]).c_str());
   }
 }

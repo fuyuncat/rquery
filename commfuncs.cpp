3c3ad63dd46f5694e0824736254a326154092cbf
fuyuncat <fuyuncat@gmail.com>
2022-07-07 17:24:07 +1000

    Expression Improvement

--- commfuncs.cpp
+++ commfuncs.cpp
@@ -480,9 +480,11 @@ bool isDate(const string& str, string& fmt)
             for (std::set<string>::iterator iz = alltzfmt.begin(); iz != alltzfmt.end(); ++iz) {
               if (strptime(str.c_str(), string((*id)+(*ij)+(*it)+(*iz)).c_str(), &tm)){
                 fmt = string((*id)+(*ij)+(*it)+(*iz));
+                trace(DEBUG, "Got date format: %s", fmt.c_str());
                 return true;
               }else if (strptime(str.c_str(), string((*it)+(*ij)+(*id)+(*iz)).c_str(), &tm)){
                 fmt = string((*it)+(*ij)+(*id)+(*iz));
+                trace(DEBUG, "Got date format: %s", fmt.c_str());
                 return true;
               }else
                 continue;

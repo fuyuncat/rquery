c030a0acaf35a9d7be860bee1882682906f3bbbb
fuyuncat <fuyuncat@gmail.com>
2022-07-07 20:17:55 +1000

    Expression Improvement

--- function.cpp
+++ function.cpp
@@ -440,7 +440,7 @@ bool FunctionC::runTruncdate(vector<string>* fieldnames, vector<string>* fieldva
       time_t t2 = (time_t)(trunc((long double)t1/iSeconds))*iSeconds;
       tm = *(localtime(&t2));
       sResult = dateToStr(tm, sFmt);
-      trace(DEBUG, "Truncating date '%s'(%u) to '%s'(%u), format:%s\n", sTm.c_str(), (long)t1, sResult.c_str(), (long)t2, sFmt.c_str());
+      //trace(DEBUG, "Truncating date '%s'(%u) to '%s'(%u), format:%s\n", sTm.c_str(), (long)t1, sResult.c_str(), (long)t2, sFmt.c_str());
       return !sResult.empty();
     }else{
       trace(ERROR, "Failed to run truncdate(%s, %s)!\n", m_params[0].m_expStr.c_str(), m_params[1].m_expStr.c_str());

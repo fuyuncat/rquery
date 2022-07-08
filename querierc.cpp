ebf800b37807283e29de5ef12455c3814a320ba3
fuyuncat <fuyuncat@gmail.com>
2022-07-08 17:41:20 +1000

    Aggregation Function improvement

--- querierc.cpp
+++ querierc.cpp
@@ -672,6 +672,7 @@ void QuerierC::mergeSort(int iLeft, int iMid, int iRight)
   if (iLeft >= iRight)
     return;
   else{
+    trace(DEBUG1, "Mergeing %d %d %d", iLeft, iMid, iRight);
     mergeSort(iLeft, (int)floor(iMid)/2, iMid);
     mergeSort(iMid+1, iMid+1+(int)floor(iRight-iMid-1)/2, iRight);
     int iLPos = iLeft, iRPos = iMid, iCheckPos = iMid;

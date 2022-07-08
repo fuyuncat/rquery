86ab7ca609f34bbe5ffa65fd76ae244a79a56e82
fuyuncat <fuyuncat@gmail.com>
2022-07-08 18:05:58 +1000

    Aggregation Function improvement

--- querierc.cpp
+++ querierc.cpp
@@ -680,10 +680,10 @@ void QuerierC::mergeSort(int iLeft, int iMid, int iRight)
       //trace(DEBUG1, "Swaping %d %d %d %d\n", iLPos, iCheckPos, iRPos, iRight);
       bool exchanged = false;
       for (int i=0; i<m_sorts.size(); i++){
-        if (m_sortKeys[m_results.begin()+iLPos][i]>m_sortKeys[m_results.begin()+iRPos][i]){
+        if (*(m_sortKeys.begin()+iLPos)[i]>*(m_sortKeys.begin()+iRPos)[i]){
           //vector<string> tmp;
-          //tmp.insert(tmp.begin(), m_results[m_results.begin()+iRPos].begin(), m_results[m_results.begin()+iRPos].end());
-          m_results.insert(m_results.begin()+iLPos,m_results[m_results.begin()+iRPos]);
+          //tmp.insert(tmp.begin(), *(m_results.begin()+iRPos).begin(), *(m_results.begin()+iRPos).end());
+          m_results.insert(m_results.begin()+iLPos,m_results.begin()+iRPos);
           m_results.erase(m_results.begin()+iRPos+1);
           exchanged = true;
           break;

10924e938a68ef3bec1963ddc159ff3ef0ae7ae6
fuyuncat <fuyuncat@gmail.com>
2022-07-08 12:01:23 +1000

    Aggregation Function improvement

--- rquery.cpp
+++ rquery.cpp
@@ -111,7 +111,7 @@ int main(int argc, char *argv[])
     }else if (boost::algorithm::to_lower_copy<string>(string(argv[i])).compare("-m")==0 || boost::algorithm::to_lower_copy<string>(string(argv[i])).compare("--msglevel")==0){
       if (encodeTracelevel(string(argv[i+1]))!=UNKNOWN){
         gv.g_tracelevel = encodeTracelevel(string(argv[i+1]));
-        trace(DEBUG,"set g_tracelevel to %d\n",g_tracelevel);
+        trace(DEBUG,"set g_tracelevel to %d\n",gv.g_tracelevel);
       }else{
         trace(FATAL,"Unrecognized message level %s. It should be one of INFO, WARNING, ERROR, FATAL.\n", argv[i]);
         return 1;

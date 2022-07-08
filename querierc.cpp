ee225569ed8333b2fbf0dbc9d1258358c66e6db5
fuyuncat <fuyuncat@gmail.com>
2022-07-08 18:18:07 +1000

    Aggregation Function improvement

--- querierc.cpp
+++ querierc.cpp
@@ -684,10 +684,10 @@ void QuerierC::mergeSort(int iLeft, int iMid, int iRight)
           //vector<string> tmp;
           //tmp.insert(tmp.begin(), (*(m_results.begin()+iRPos)).begin(), (*(m_results.begin()+iRPos)).end());
           trace(DEBUG1, "moving %s before %s\n", (*(m_sortKeys.begin()+iRPos))[i].c_str(), (*(m_sortKeys.begin()+iLPos))[i].c_str());
-          trace(DEBUG1, "Before move: %s before %s\n", (*(m_results.begin()+iRPos))[2].c_str(), (*(m_results.begin()+iLPos))[2].c_str());
+          trace(DEBUG1, "Before move: %s(%d) before %s(%d)\n", (*(m_results.begin()+iLPos))[2].c_str(), iLPos, (*(m_results.begin()+iRPos))[2].c_str(), iLPos);
           m_results.insert(m_results.begin()+iLPos,*(m_results.begin()+iRPos));
           m_results.erase(m_results.begin()+iRPos+1);
-          trace(DEBUG1, "After move: %s before %s\n", (*(m_results.begin()+iRPos))[2].c_str(), (*(m_results.begin()+iLPos))[2].c_str());
+          trace(DEBUG1, "After move: %s(%d) before %s(%d)\n", (*(m_results.begin()+iLPos))[2].c_str(), iLPos, (*(m_results.begin()+iRPos))[2].c_str(, iLPos));
           exchanged = true;
           break;
         }
